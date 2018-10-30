c036b3594ec9cbe0a29f3d3ad669044a35a555c7
TeemuValonen <43816558+TeemuValonen@users.noreply.github.com>
2018-10-30 16:10:13 +0200

    Update Tekstieditori.java
    avaa/tallenna korjattu

--- Tekstieditori.java
+++ Tekstieditori.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -11,6 +12,7 @@ import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.awt.event.InputEvent;
 import javax.swing.JToolBar;
 import javax.swing.JButton;
@@ -21,19 +23,17 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import java.util.Scanner;
+import javax.swing.JTextField;
 
 public class Tekstieditori extends JFrame {
 
-	
-	
 	private JPanel contentPane;
-	private static JEditorPane editorPane;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -44,9 +44,6 @@ public class Tekstieditori extends JFrame {
 				}
 			}
 		});
-		
-		
-
 
 	}
 
@@ -54,48 +51,77 @@ public class Tekstieditori extends JFrame {
 	 * Create the frame.
 	 */
 	public Tekstieditori() {
+		JEditorPane editorPane = new JEditorPane();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnTiedosto = new JMenu("Tiedosto");
 		menuBar.add(mnTiedosto);
-		
+
 		JMenuItem mntmAvaa = new JMenuItem("Avaa");
 		mntmAvaa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				JFileChooser valintaikkuna = new JFileChooser();
-		        valintaikkuna.showOpenDialog(null);
-		        String rivi = "";
-		        String uusiTiedosto = valintaikkuna.getSelectedFile().getAbsolutePath();
-				
-				Scanner lukija = null;
-				File tiedosto = new File(uusiTiedosto);
-				
+				valintaikkuna.showOpenDialog(null);
+				String rivi = "";
+				String uusiTiedosto = valintaikkuna.getSelectedFile().getAbsolutePath();
+
 				try {
+					Scanner lukija = null;
+					File tiedosto = new File(uusiTiedosto);
+
 					lukija = new Scanner(tiedosto);
-					
+
 					while (lukija.hasNextLine()) {
-						rivi += lukija.nextLine()+"\n";
+						rivi += lukija.nextLine() + "\n";
 						System.out.println(rivi);
 					}
+
 				} catch (FileNotFoundException p) {
 					System.out.println("Tiedostoa ei löydy");
 				}
 				editorPane.setText(rivi);
-				
+
 			}
 		});
 		mntmAvaa.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
 		mnTiedosto.add(mntmAvaa);
-		
+
 		JMenuItem mntmTallenna = new JMenuItem("Tallenna");
+		mntmTallenna.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				JFileChooser valintaikkuna = new JFileChooser();
+				valintaikkuna.showSaveDialog(null);
+
+				String uusiTiedosto = valintaikkuna.getSelectedFile().getAbsolutePath();
+
+				System.out.println("Kirjoitettava tiedosto: " + uusiTiedosto);
+
+				try {
+					PrintWriter writer = new PrintWriter(uusiTiedosto);
+					String sisalto = editorPane.getText();
+
+					writer.println(sisalto);
+					
+					writer.flush();
+					writer.close();
+
+				} catch (Exception e1) {
+					System.out.println("Tiedoston tallennuksessa tapahtui virhe!");
+					e1.printStackTrace();
+				}
+
+			}
+		});
 		mntmTallenna.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
 		mnTiedosto.add(mntmTallenna);
-		
+
 		JMenuItem mntmLopeta = new JMenuItem("Lopeta");
 		mntmLopeta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -103,43 +129,68 @@ public class Tekstieditori extends JFrame {
 			}
 		});
 		mnTiedosto.add(mntmLopeta);
-		
+
 		JMenuItem mntmSulje = new JMenuItem("Sulje");
 		mnTiedosto.add(mntmSulje);
-		
+
 		JMenu mnMuokkaa = new JMenu("Muokkaa");
 		menuBar.add(mnMuokkaa);
-		
+
 		JMenuItem mntmEtsi = new JMenuItem("Etsi");
+		mntmEtsi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String sisalto = editorPane.getText();
+				sisalto = sisalto.toLowerCase();
+				
+				String haettava = "auto";
+				int indeksi = sisalto.indexOf(haettava);
+				
+				System.out.println("Indeksi: "+indeksi);
+				
+				editorPane.setSelectionColor(Color.MAGENTA);
+				
+				editorPane.setSelectionStart(indeksi);
+				editorPane.setSelectionEnd( indeksi + haettava.length() );
+				
+			}
+		});
 		mnMuokkaa.add(mntmEtsi);
-		
+
 		JMenuItem mntmKorvaa = new JMenuItem("Korvaa");
 		mnMuokkaa.add(mntmKorvaa);
-		
+
 		JMenu mnTietoja = new JMenu("Tietoja");
 		menuBar.add(mnTietoja);
-		
+
 		JMenuItem mntmTietojaOhjelmasta = new JMenuItem("Tietoja ohjelmasta");
 		mnTietoja.add(mntmTietojaOhjelmasta);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JToolBar toolBar = new JToolBar();
-		toolBar.setBounds(5, 5, 424, 29);
+		toolBar.setBounds(0, 0, 429, 34);
 		contentPane.add(toolBar);
-		
+
 		JButton button = new JButton("");
 		button.setIcon(new ImageIcon(Tekstieditori.class.getResource("/javax/swing/plaf/metal/icons/ocean/file.gif")));
 		toolBar.add(button);
-		
+
 		JButton button_1 = new JButton("");
-		button_1.setIcon(new ImageIcon(Tekstieditori.class.getResource("/javax/swing/plaf/metal/icons/ocean/floppy.gif")));
+		button_1.setIcon(
+				new ImageIcon(Tekstieditori.class.getResource("/javax/swing/plaf/metal/icons/ocean/floppy.gif")));
 		toolBar.add(button_1);
-		
+
 		JButton button_2 = new JButton("");
-		button_2.setIcon(new ImageIcon(Tekstieditori.class.getResource("/com/sun/javafx/scene/control/skin/modena/HTMLEditor-Cut-Black.png")));
+		button_2.setIcon(new ImageIcon(
+				Tekstieditori.class.getResource("/com/sun/javafx/scene/control/skin/modena/HTMLEditor-Cut-Black.png")));
 		toolBar.add(button_2);
+		
+		
+		editorPane.setBounds(0, 32, 434, 200);
+		contentPane.add(editorPane);
+
 	}
 }
